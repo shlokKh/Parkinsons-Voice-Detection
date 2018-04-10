cf27db5c6f8ec1e1e644152d8039b79ed375539a
shlokKh <shlok@shlok.net>
2018-04-10 14:30:30 -0400

    Add files via upload

---
+++ parkinsonsdata.py
@@ -0,0 +1,104 @@
+import pandas as pd 
+import numpy as np 
+import os, sys
+
+data = pd.read_csv('parkinsons.data')
+predictors = data.drop(['name'], axis = 1)
+predictors = predictors.drop(['status'], axis = 1).as_matrix()
+target = data['status']
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler((-1, 1))
+X = scaler.fit_transform(predictors)
+Y = target
+
+import xgboost as xgb
+from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = .25, random_state = 7)
+
+model = XGBClassifier()
+model.fit(X_train, Y_train)
+
+y_pred = model.predict(X_test)
+print("XGB boost:")
+print(metrics.accuracy_score(Y_test, y_pred))
+print(metrics.classification_report(Y_test, y_pred))
+print(metrics.confusion_matrix(Y_test, y_pred))
+
+
+# Logistic Regression
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+# load the iris datasets
+dataset = datasets.load_iris()
+# fit a logistic regression model to the data
+model = LogisticRegression()
+model.fit(X_train, Y_train)
+y_pred = model.predict(X_test)
+# summarize the fit of the model
+print("Logistic Regression: ")
+print(metrics.accuracy_score(Y_test, y_pred))
+print(metrics.classification_report(Y_test, y_pred))
+print(metrics.confusion_matrix(Y_test, y_pred))
+
+# Gaussian Naive Bayes
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB
+model = GaussianNB()
+model.fit(X_train, Y_train)
+# make predictions
+y_pred = model.predict(X_test)
+# summarize the fit of the model
+print("Gaussian Naive Bayes:")
+print(metrics.accuracy_score(Y_test, y_pred))
+print(metrics.classification_report(Y_test, y_pred))
+print(metrics.confusion_matrix(Y_test, y_pred))
+
+
+#K-Nearest Neighbor (BEST ONE 98%%)
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+model = KNeighborsClassifier()
+model.fit(X_train, Y_train)
+# make predictions
+y_pred = model.predict(X_test)
+# summarize the fit of the model
+print("k-Nearest Neighbor: ")
+print(metrics.accuracy_score(Y_test, y_pred))
+print(metrics.classification_report(Y_test, y_pred))
+print(metrics.confusion_matrix(Y_test, y_pred))
+
+# Support Vector Machine
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.svm import SVC
+model = SVC()
+model.fit(X_train, Y_train)
+
+# make predictions
+y_pred = model.predict(X_test)
+# summarize the fit of the model
+print("Support Vector Machine: ")
+print(metrics.accuracy_score(Y_test, y_pred))
+print(metrics.classification_report(Y_test, y_pred))
+print(metrics.confusion_matrix(Y_test, y_pred))
+
+# Classification and Regression Trees
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.tree import DecisionTreeClassifier
+model = DecisionTreeClassifier()
+model.fit(X_train, Y_train)
+# make predictions
+y_pred = model.predict(X_test)
+# summarize the fit of the model
+print("Classification and Regression Trees")
+print(metrics.accuracy_score(Y_test, y_pred))
+print(metrics.classification_report(Y_test, y_pred))
+print(metrics.confusion_matrix(Y_test, y_pred))
